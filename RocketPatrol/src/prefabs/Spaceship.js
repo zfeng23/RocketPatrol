a818fe836ef282d9a9c48e0462f6f0c55f03de06
Zhenglin Feng <zfeng23@ucsc.edu>
2022-04-18 23:59:58 +0800

    commit

--- RocketPatrol/src/prefabs/Spaceship.js
+++ RocketPatrol/src/prefabs/Spaceship.js
@@ -4,7 +4,7 @@ class Spaceship extends Phaser.GameObjects.Sprite {
         super(scene, x, y, texture, frame);
         scene.add.existing(this);   // add to existing scene
         this.points = pointValue;   // store pointValue
-        this.moveSpeed = game.settings.spaceshipSpeed;         // pixels per frame
+        this.moveSpeed = game.settings.spaceshipSpeed + 2;         // pixels per frame
     }
 
     update() {
